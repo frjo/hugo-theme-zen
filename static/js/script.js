8dfb9c8763633fb4039141621ac74b910ada58ff
Fredrik Jonsson <frjo@xdeb.org>
2017-07-25 11:18:16 +0200

    Add the mobile menu directly in baseof template instead of via js. Translate menu terms.

--- static/js/script.js
+++ static/js/script.js
@@ -10,21 +10,21 @@
   // Add a js class to the html-tag when JavsScript is active.
   $('html').addClass('js');
 
-  // Activate the SlickNav menu for small screens.
+  // Activate the mobil menu for small screens.
   if (window.matchMedia) {
     var mq = window.matchMedia('(max-width: 666px)');
     if (mq.matches) {
-      // Create the nav toggle button and the nav sheet.
-      $('.page').after('<div class="js-nav-wrapper"><div class="js-nav-cover"></div><a href="#navigation" class="js-nav-toggle" aria-haspopup="true" role="button">Menu</a><div class="js-nav-sheet"></div></div>');
+      // Show mobile menu.
+      $('.mobile-nav-wrapper').removeClass('hidden');
 
-      // Populate the nav sheet.
-      $('.main-menu').addClass('hidden').clone().removeClass('main-menu layout__navigation layout__full hidden hidden').addClass('js-main-menu').appendTo('.js-nav-sheet').find('.navbar').removeClass('navbar').addClass('js-navbar');
-      $('.header__region').addClass('hidden').clone().removeClass('header__region region hidden').addClass('js-region').appendTo('.js-nav-sheet');
+      // Populate the mobile nav sheet.
+      $('.main-menu').addClass('hidden').clone().removeClass('main-menu layout__navigation layout__full hidden hidden').addClass('js-main-menu').appendTo('.mobile-nav-sheet').find('.navbar').removeClass('navbar').addClass('js-navbar');
+      $('.header__region').addClass('hidden').clone().removeClass('header__region region hidden').addClass('js-region').appendTo('.mobile-nav-sheet');
 
-      // Toggle the nav sheet.
-      $('.js-nav-cover, .js-nav-toggle').on('click toushstart', function (e) {
+      // Toggle the mobile nav sheet.
+      $('.mobile-nav-cover, .mobile-nav-toggle').on('click toushstart', function (e) {
         e.preventDefault();
-        $('.js-nav-wrapper').toggleClass('js-nav-open');
+        $('.mobile-nav-wrapper').toggleClass('js-nav-open');
       });
 
     }
