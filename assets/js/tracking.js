79b366cb1860e00b2f1d370af3f32e729eb82985
Fredrik Jonsson <frjo@xdeb.org>
2021-01-29 14:53:38 +0100

    Cookie concent feature.

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,16 +3,29 @@
  * A JavaScript file for analytic tracking.
  */
 
+var cookieConsent = {{ .Site.Params.cookieConsent | default false }};
+
 var idSite = '{{ .Site.Params.piwikSiteID }}';
 var matomoTrackingApiUrl = 'https://{{ .Site.Params.piwikTrackerUrl }}/matomo.php';
 
 var _paq = window._paq || [];
+if (cookieConsent) {
+  _paq.push(['requireConsent']);
+}
 _paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
 _paq.push(['setSiteId', idSite]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+if (typeof window.Cookies !== 'undefined' && Cookies.get('cookieconsent') === 'accept') {
+  _paq.push(['setConsentGiven']);
+}
 
 var googleAnalytics = '{{ .Site.GoogleAnalytics }}';
+if (cookieConsent) {
+  if (typeof window.Cookies === 'undefined' || Cookies.get('cookieconsent') !== 'accept') {
+    window['ga-disable-' + googleAnalytics] = true;
+  }
+}
 
 var _gaq = window._gaq || [];
 _gaq.push(['_setAccount', googleAnalytics]);
