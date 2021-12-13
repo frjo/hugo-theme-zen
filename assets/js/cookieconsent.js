767119d014b08b92ca57f4d048a1cea3da1c5229
Fredrik Jonsson <frjo@xdeb.org>
2021-12-13 10:50:23 +0100

    Use localstorage for cookiconsent response instead of setting a cookie.

--- assets/js/cookieconsent.js
+++ assets/js/cookieconsent.js
@@ -3,39 +3,29 @@
 
   'use strict';
 
-  if (typeof Cookies !== 'undefined') {
-    const cookieconsent = document.querySelector('.cookieconsent');
+  const cookiebanner = document.querySelector('.cookieconsent');
+  const cookieconsent = localStorage.getItem('cookieconsent');
 
-    if (typeof Cookies.get('cookieconsent') === 'undefined' && cookieconsent) {
-      cookieconsent.classList.remove('hidden');
-      cookieconsent.classList.add('js-cookieconsent-open');
-    }
-
-    const cookie_buttons = Array.prototype.slice.call(
-      document.querySelectorAll('button[data-consent]')
-    );
-    const sitedomain = window.location.hostname.split('.').slice(-2);
-    const cookiedomain = sitedomain.join('.');
-    let cookie_options = [];
-    cookie_options['domain'] = cookiedomain;
-    cookie_options['sameSite'] = 'strict';
-    cookie_options['expires'] = 365;
-    if (window.location.protocol === 'https:') {
-      cookie_options['secure'] = true;
-    }
-
-    cookie_buttons.forEach(function (button) {
-      button.addEventListener('click', function () {
-        if (button.getAttribute('data-consent') === 'true') {
-          Cookies.set('cookieconsent', 'accept', cookie_options);
-        }
-        else {
-          Cookies.set('cookieconsent', 'decline', cookie_options);
-        }
-        cookieconsent.classList.remove('js-cookieconsent-open');
-        cookieconsent.classList.add('hidden');
-      });
-    });
+  if (cookiebanner && !cookieconsent) {
+    cookiebanner.classList.remove('hidden');
+    cookiebanner.classList.add('js-cookieconsent-open');
   }
+
+  const cookie_buttons = Array.prototype.slice.call(
+    document.querySelectorAll('button[data-consent]')
+  );
+
+  cookie_buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      if (button.getAttribute('data-consent') === 'true') {
+        localStorage.setItem('cookieconsent', 'accept');
+      }
+      else {
+        localStorage.setItem('cookieconsent', 'decline');
+      }
+      cookiebanner.classList.remove('js-cookieconsent-open');
+      cookiebanner.classList.add('hidden');
+    });
+  });
 
 })();
--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -5,26 +5,26 @@
  * A JavaScript file for analytic tracking.
  */
 
-var cookieConsent = {{ .Site.Params.cookieConsent | default false }};
-
+var cookiebanner = {{ .Site.Params.cookieConsent | default false }};
+var cookieconsent = localStorage.getItem('cookieconsent');
 var idSite = '{{ .Site.Params.piwikSiteID }}';
 var matomoTrackingApiUrl = 'https://{{ .Site.Params.piwikTrackerUrl }}/matomo.php';
-
-var _paq = window._paq || [];
-if (cookieConsent) {
-  _paq.push(['requireConsent']);
-}
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-if (typeof Cookies !== 'undefined' && Cookies.get('cookieconsent') === 'accept') {
-  _paq.push(['setConsentGiven']);
-}
-
 var googleAnalytics = '{{ .Site.GoogleAnalytics }}';
-if (cookieConsent) {
-  if (typeof Cookies === 'undefined' || Cookies.get('cookieconsent') !== 'accept') {
-    window['ga-disable-' + googleAnalytics] = true;
+
+if (idSite) {
+  var _paq = window._paq || [];
+  if (cookiebanner) {
+    _paq.push(['requireConsent']);
   }
+  _paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
+  _paq.push(['setSiteId', idSite]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  if (cookiebanner && cookieconsent === 'accept') {
+    _paq.push(['setConsentGiven']);
+  }
+}
+
+if (googleAnalytics && cookiebanner && cookieconsent === 'decline') {
+  window['ga-disable-' + googleAnalytics] = true;
 }
