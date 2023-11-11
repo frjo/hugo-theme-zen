51f594091bfcb00332acd2da0e12201c84d83a37
Fredrik Jonsson <frjo@xdeb.org>
2023-11-11 10:58:14 +0100

    Lint js fix.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -14,21 +14,21 @@
   const activateForm = function () {
     form.setAttribute('action', action);
     button.removeAttribute('disabled');
-  }
+  };
 
   // Display the hidden form.
   form.classList.remove('hidden');
 
   // Wait for a mouse to move, indicating they are human.
-  document.body.addEventListener('mousemove', activateForm, { once: true });
+  document.body.addEventListener('mousemove', activateForm, {once: true});
   // Wait for a touch move event, indicating that they are human.
-  document.body.addEventListener('touchmove', activateForm, { once: true });
+  document.body.addEventListener('touchmove', activateForm, {once: true});
   // A tab or enter key pressed can also indicate they are human.
   document.body.addEventListener('keydown', function (e) {
     if ((e.key === 'Tab') || (e.key === 'Enter')) {
       activateForm();
     }
-  }, { once: true });
+  }, {once: true});
 
   // Mark the form as submitted.
   button.addEventListener('click', () => form.classList.add('js-submitted'));
